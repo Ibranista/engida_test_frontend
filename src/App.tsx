6780d77b5e73f7d94dc5850e66f5b76413905a33
ibrahim kedir <techofreact@gmail.com>
2025-03-08 15:03:45 +0300

    git commit -m "fix: root not displaying task managment component."

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import 'antd/dist/antd.css';
+import 'antd/dist/reset.css'
+import './index.css';
+import TaskManager from './components/Engida.Tasks.Manager';
 
+const App: React.FC = () => {
   return (
-    <>
-      Hello
-    </>
-  )
-}
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<TaskManager />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
